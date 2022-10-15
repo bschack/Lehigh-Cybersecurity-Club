30c8acfd6d7bbd8e74053b3cb1ab85b69c72c023
bschack <benharris1102@gmail.com>
2022-10-14 23:30:09 -0400

    homepage init

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,7 +1,14 @@
+import styles from "./homepage.module.scss";
+
 export const Homepage = () => {
   return (
     <div>
-      Homepage
+      <div className={styles['homepage__section']}>
+        <div className={styles['homepage__section-header']} />
+      </div>
+      <div className={styles['homepage__section']}>
+        <div className={styles['homepage__description']}>We are the Lehigh University Cybersecurity Club</div>
+      </div>
     </div>
   );
 };
